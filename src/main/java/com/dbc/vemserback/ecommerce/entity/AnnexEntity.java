13d4d315402e4e9ac247c311f83914b0cf4c89a9
Luiz Souza <luixsouza12@outlook.com>
2022-04-21 19:39:17 -0300

    alterando entidades annex, purchase para mongodb

--- src/main/java/com/dbc/vemserback/ecommerce/entity/AnnexEntity.java
+++ src/main/java/com/dbc/vemserback/ecommerce/entity/AnnexEntity.java
@@ -3,28 +3,21 @@ package com.dbc.vemserback.ecommerce.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
-@Entity(name = "ANNEX")
+@Document(collection = "ANNEX")
 public class AnnexEntity {
 
     @Id
-    @Column(name = "annex_id", columnDefinition = "serial")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer annexId;
+    private String annexId;
 
-    @Column(name = "list_id", insertable = false, updatable = false)
     private Integer listId;
 
-    @Column(name = "title")
     private String title;
 
-    @JsonIgnore
-    @ManyToOne
-    @JoinColumn(referencedColumnName = "list_id", name = "list_id")
-    private PucharseListEntity pucharseListEntity;
 
 }
--- src/main/java/com/dbc/vemserback/ecommerce/entity/PucharseListEntity.java
+++ src/main/java/com/dbc/vemserback/ecommerce/entity/PucharseListEntity.java
@@ -3,40 +3,33 @@ package com.dbc.vemserback.ecommerce.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 @Getter
 @Setter
-@Entity(name = "PUCHARSE_LIST")
+@Document(collection = "PUCHARSE_LIST")
 public class PucharseListEntity {
 
     @Id
-    @Column(name= "list_id", columnDefinition = "serial")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer listId;
+    private String listId;
 
-    @Column(name = "topic_id", insertable = false, updatable = false)
     private Integer topicId;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "date_topic")
     private LocalDate date;
 
-    @Column(name = "total_value")
     private Double totalValue;
 
-
-    @JsonIgnore
-    @ManyToOne
-    @JoinColumn(referencedColumnName = "topic_id", name = "topic_id")
+    @DBRef
     private TopicEntity topicEntity;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "pucharseListEntity")
-    private Set<AnnexEntity> annexEntities;
+    @DBRef
+    private List<AnnexEntity> annexEntities;
 }
