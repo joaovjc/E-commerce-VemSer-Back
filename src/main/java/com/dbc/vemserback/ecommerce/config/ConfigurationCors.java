be03c688c2b50199b4511f67bf7ad032c2a98893
Your Name <you@example.com>
2022-04-20 23:01:45 -0300

    terminando o login

--- src/main/java/com/dbc/vemserback/ecommerce/config/ConfigurationCors.java
+++ src/main/java/com/dbc/vemserback/ecommerce/config/ConfigurationCors.java
@@ -1,8 +1,10 @@
 package com.dbc.vemserback.ecommerce.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class ConfigurationCors implements WebMvcConfigurer {
 	
 	@Override
