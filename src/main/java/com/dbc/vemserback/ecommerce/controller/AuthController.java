4c177d79368e5f6c8c103aee9f2e37f79966a903
Your Name <you@example.com>
2022-04-22 11:27:25 -0300

    revisando os codigos de login e tratando exceções

--- src/main/java/com/dbc/vemserback/ecommerce/controller/AuthController.java
+++ src/main/java/com/dbc/vemserback/ecommerce/controller/AuthController.java
@@ -5,12 +5,11 @@ import javax.validation.Valid;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dbc.vemserback.ecommerce.dto.CreateUserDTO;
