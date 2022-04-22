4a355bb69aadedaa4dcd181e4ff357bacb12de4d
Your Name <you@example.com>
2022-04-22 11:31:47 -0300

    revisando os codigos de login e tratando exceções

--- src/main/java/com/dbc/vemserback/ecommerce/controller/AuthController.java
+++ src/main/java/com/dbc/vemserback/ecommerce/controller/AuthController.java
@@ -5,11 +5,12 @@ import javax.validation.Valid;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dbc.vemserback.ecommerce.dto.CreateUserDTO;
@@ -44,13 +45,12 @@ public class AuthController {
     }
     
     @PostMapping("/sign-up")
-    public UserLoginDto signUp(@Valid @RequestPart(name = "data") CreateUserDTO CreateDTO) throws BusinessRuleException {
-//    public UserLoginDto signUp(@Valid @ModelAttribute(name = "data") CreateUserDTO CreateDTO, BindingResult bindingResult) throws BusinessRuleException {
-//    	if(bindingResult.hasErrors()) {
-//    		StringBuilder builder = new StringBuilder();
-//        	bindingResult.getAllErrors().forEach(err -> builder.append(err.getDefaultMessage()));
-//    		throw new BusinessRuleException(builder.toString());
-//    	}
+    public UserLoginDto signUp(@Valid @ModelAttribute(name = "data") CreateUserDTO CreateDTO, BindingResult bindingResult) throws BusinessRuleException {
+    	if(bindingResult.hasErrors()) {
+    		StringBuilder builder = new StringBuilder();
+        	bindingResult.getAllErrors().forEach(err -> builder.append(err.getDefaultMessage()));
+    		throw new BusinessRuleException(builder.toString());
+    	}
     	
     	UserLoginDto createUser = userService.createUser(CreateDTO);
     	
