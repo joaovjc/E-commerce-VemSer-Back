74740ba9fd6184a50215c039bfdbdc149bd7a228
Your Name <you@example.com>
2022-04-23 16:52:13 -0300

    arrumando query

--- src/main/java/com/dbc/vemserback/ecommerce/repository/mongo/TopicRepository.java
+++ src/main/java/com/dbc/vemserback/ecommerce/repository/mongo/TopicRepository.java
@@ -2,10 +2,10 @@ package com.dbc.vemserback.ecommerce.repository.mongo;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dbc.vemserback.ecommerce.dto.topic.TopicAgreg;
@@ -23,5 +23,5 @@ public interface TopicRepository extends MongoRepository<TopicEntity, String> {
 //	Page<TopicAgreg> findAllTopicsByIdUser(int idUser, Pageable pageable);
 	
 	@Query(fields = "{purchases : 1}")
-	List<String> findAllPurchasesIdByIdTopicAndIdUser(String idTopic, int idUser);
+	List<String> findAllPurchasesIdByIdTopicAndIdUser(@Param(value = "_id") String idTopic, int idUser);
 }
